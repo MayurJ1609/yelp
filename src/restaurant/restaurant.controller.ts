c3e7bfd3d4b8d05b0f7831bafdbf708d3c38764f
MayurJ1609 <mayur.jaganmohan@gmail.com>
2020-06-15 18:51:05 +0530

    Added CRUD methods to restaurant controller

--- src/restaurant/restaurant.controller.ts
+++ src/restaurant/restaurant.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Logger, Get } from '@nestjs/common';
+import {
+  Controller,
+  Logger,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { GetRestaurantFilterDto } from './dto/get-restaurants-filter-dto';
+import { Restaurant } from './restaurant.schema';
+import { CreateRestaurantDto } from './dto/create-restaurant-dto';
 
 @Controller('restaurant')
 export class RestaurantController {
@@ -9,7 +21,34 @@ export class RestaurantController {
   constructor(private restaurantService: RestaurantService) {}
 
   @Get()
-  getRestaurants(filterDto: GetRestaurantFilterDto): Promise<Restaurant[]>{
+  getRestaurants(@Query() filterDto: GetRestaurantFilterDto) {
+    this.logger.verbose(`Filter "${JSON.stringify(filterDto)}"`);
+    return this.restaurantService.getRestaurants(filterDto);
+  }
 
+  @Get(':id')
+  getRestaurant(@Param('id') restaurantId: string) {
+    return this.restaurantService.getRestaurantById(restaurantId);
+  }
+
+  @Post()
+  createRestaurant(@Body() createRestaurantDto: CreateRestaurantDto) {
+    return this.restaurantService.createRestaurant(createRestaurantDto);
+  }
+
+  @Patch(':id')
+  updateRestaurant(
+    @Param('id') restaurantId: string,
+    @Body() createRestaurantDto: CreateRestaurantDto,
+  ) {
+    return this.restaurantService.updateRestaurant(
+      restaurantId,
+      createRestaurantDto,
+    );
+  }
+
+  @Delete(':id')
+  deleteRestaurant(@Param('id') restaurantId: string) {
+    return this.restaurantService.deleteRestaurant(restaurantId);
   }
 }
